19ac07ea16f340debe903e6ed74921ec00cb3b6b
Anuraj <101694416+Anuraj-TR@users.noreply.github.com>
2022-07-19 12:40:16 +0530

    testimonials completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
+$(".testimonial__card-wrapper").slick({
+  dots: true,
+  infinite: true,
+  speed: 600,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: false,
+  centerMode: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        centerMode: false,
+      },
+    },
+  ],
+});
+
 $(".blog-cards").slick({
   dots: true,
   infinite: true,
@@ -30,9 +68,6 @@ $(".blog-cards").slick({
         slidesToScroll: 1,
       },
     },
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ],
 });
 
